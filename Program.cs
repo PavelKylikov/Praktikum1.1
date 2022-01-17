786733f58c83a03975285fc91b5ef70dcf156356
PavelKylikov <pawel-kkk@yandex.ru>
2022-01-17 22:02:30 +0300

    Создали массив

--- Program.cs
+++ Program.cs
@@ -1,5 +1,31 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿// Метод наполнения массива случайными (Fill) числами от [1 до 10).
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+// Метод вывода (Print) в консоль массива.
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+         Console.Write(col[position] + " ");
+         position++;
+    }
+}
+
+// Команда: Создай новый массив array в котором будет 10 элементов 
+int[] array = new int[10];
+// Заполняем массив array случайными числами.
+FillArray(array);
+// Выводим массив array в консоль.
+PrintArray(array);
+Console.WriteLine();
